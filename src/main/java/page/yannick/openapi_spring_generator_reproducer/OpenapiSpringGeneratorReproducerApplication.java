6a247776ffbecc1bf34bcc00659a5fb8bda948fa
Yannick Pfaff <yannick.pfaff16@gmail.com>
2025-08-20 02:24:31 +0200

    chore: init

---
+++ src/main/java/page/yannick/openapi_spring_generator_reproducer/OpenapiSpringGeneratorReproducerApplication.java
@@ -0,0 +1,13 @@
+package page.yannick.openapi_spring_generator_reproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiSpringGeneratorReproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiSpringGeneratorReproducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/page/yannick/openapi_spring_generator_reproducer/OpenapiSpringGeneratorReproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package page.yannick.openapi_spring_generator_reproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSpringGeneratorReproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
